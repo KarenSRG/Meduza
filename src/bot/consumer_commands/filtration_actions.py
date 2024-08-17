1b7c28699753eba5ea57df7ff2bc5cc169346398
KarenSRG <1865030a@gmail.com>
2024-08-17 09:42:31 +0400

    Message Pagination.

--- src/bot/consumer_commands/filtration_actions.py
+++ src/bot/consumer_commands/filtration_actions.py
@@ -20,23 +20,32 @@ async def command_actions(message: Message) -> None:
                          "\n\n".join([f"{action[0]}  ->  {action[1]}" for action in actions]))
 
 
-def query_parser(message: Message) -> str or Awaitable:
-    if is_active_producer(message.from_user.id):
+async def long_answer(message: Message, text: str) -> None:
+    if len(text) > 4096:
+        parts = [text[i:i + 4096] for i in range(0, len(text), 4096)]
+        for part in parts:
+            await message.answer(part)
+    else:
+        await message.answer(text)
+
+
+async def query_parser(message: Message) -> str or Awaitable:
+    if await is_active_producer(message.from_user.id):
         text = message.text.split()
-        if len(text) == 2:
-            return text[1]
+        if len(text) > 2:
+            return message.text[message.text.index(" "):].strip()
         return message.answer(f"Invalid {html.bold('query')}.")
     return message.answer(f"This {html.bold('command')} not allowed for you.")
 
 
 async def command_startswith(message: Message) -> None:
-    result = query_parser(message)
+    result = await query_parser(message)
     if type(result) is str:
         message_list = await dao.message.startswith(result)
         if message_list:
             answer_template = f"Messages starting starting with {html.bold(result)}:\n\n\n" + \
                               "\n".join(message.text for message in message_list)
-            await message.answer(answer_template)
+            await long_answer(message, answer_template)
         else:
             await message.answer(f"There are not messages starting with {html.bold(result)}.")
     else:
@@ -44,13 +53,13 @@ async def command_startswith(message: Message) -> None:
 
 
 async def command_contains(message: Message) -> None:
-    result = query_parser(message)
+    result = await query_parser(message)
     if type(result) is str:
         message_list = await dao.message.contains(result)
         if message_list:
             answer_template = f"Messages, that contains {html.bold(result)}:\n\n\n" + \
                               "\n".join(message.text for message in message_list)
-            await message.answer(answer_template)
+            await long_answer(message, answer_template)
         else:
             await message.answer(f"There are not messages, that contains {html.bold(result)}.")
     else:
@@ -58,13 +67,13 @@ async def command_contains(message: Message) -> None:
 
 
 async def command_fromchat(message: Message) -> None:
-    result = query_parser(message)
+    result = await query_parser(message)
     if type(result) is str:
         message_list = await dao.message.fromchat(result)
         if message_list:
             answer_template = f"Messages from chat {html.bold(result)}:\n\n\n" + \
                               "\n".join(message.text for message in message_list)
-            await message.answer(answer_template)
+            await long_answer(message, answer_template)
         else:
             await message.answer(f"There are not messages from {html.bold(result)}.")
     else:
@@ -72,13 +81,13 @@ async def command_fromchat(message: Message) -> None:
 
 
 async def command_fromuser(message: Message) -> None:
-    result = query_parser(message)
+    result = await query_parser(message)
     if type(result) is str:
         message_list = await dao.message.fromuser(result)
         if message_list:
             answer_template = f"Messages from user {html.bold(result)}:\n\n\n" + \
                               "\n".join(message.text for message in message_list)
-            await message.answer(answer_template)
+            await long_answer(message, answer_template)
         else:
             await message.answer(f"There are not messages from {html.bold(result)}.")
     else:
--- src/permissions.py
+++ src/permissions.py
@@ -1,2 +1,10 @@
-def is_active_producer(telegram_id):
-    return True
+from sqlalchemy.exc import NoResultFound
+
+from src.database.facade import dao
+
+
+async def is_active_producer(telegram_id: int) -> bool:
+    try:
+        return (await dao.producer.retrieve(telegram_id)).active
+    except NoResultFound:
+        return True
